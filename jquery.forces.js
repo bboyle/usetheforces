7726ce626e348aa69ce9282823c7658782e3bc58
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-03-29 07:05:06 +0000

    :-xf-input selector support

---
+++ jquery.forces.js
@@ -0,0 +1,14 @@
+// jquery.forces.js
+// usetheforces.googlecode.com
+
+(function($){
+
+// selectors
+$.extend($.expr[':'], {
+	'-xf-input': function(e) {
+		return $(e).hasClass('xf-input');
+	}
+});
+
+
+})(jQuery);
