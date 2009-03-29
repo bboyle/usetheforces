83fae15464d4dfc90b4163971845052a4aab43b2
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-03-29 07:18:35 +0000

    added :-xf-hint and :-xf-label selectors

--- jquery.forces.js
+++ jquery.forces.js
@@ -5,9 +5,15 @@
 
 // selectors
 $.extend($.expr[':'], {
+	'-xf-hint': function(e) {
+		return $(e).hasClass('xf-hint');
+	},
 	'-xf-input': function(e) {
 		return $(e).hasClass('xf-input');
-	}
+	},
+	'-xf-label': function(e) {
+		return $(e).hasClass('xf-label');
+	},
 });
 
 
