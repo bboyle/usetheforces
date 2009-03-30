69b2b5ef4a7577d68be30441ffe8603315c7e1b0
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-03-30 12:43:24 +0000

    implement relevant, with demo page http://code.google.com/p/usetheforces/wiki/Relevant

--- jquery.forces.forms.js
+++ jquery.forces.forms.js
@@ -20,4 +20,58 @@ $.extend($.expr[':'], {
 });
 
 
+$.fn.extend({
+	// get form control
+	formControl: function() {
+		return this.is(':-xf-control') ? this : this.parents(':-xf-control').eq(0);
+	},
+
+	// recalculate
+	recalculate: function() {
+		if (this.relevant()) this.show();
+		else this.hide();
+	},
+
+	// get/set relevance
+	relevant: function(expression) {
+		var formControl = this.formControl();
+		if (expression) {
+			formControl.data('relevant', expression);
+		} else {
+			expression = formControl.data('relevant', expression);
+		}
+
+		var relevant = true;
+		if (typeof(expression) == "boolean") {
+			relevant = expression;
+		} else if (typeof(expression) == "function") {
+			relevant = expression(this) == true;
+		}
+		
+		if (!relevant) {
+			// TODO +filter from jquery object
+			formControl.hide()
+			return false;
+		}
+		
+		// default
+		formControl.show();
+		return true;
+	}
+});
+
+
+// form control changes
+$('form.forces').change(function(eventObject) {
+
+	var formControl = $(eventObject.target).formControl();
+
+	// recalculate all controls
+	// TODO rather than use "each", make functions work with jQuery object
+	$(':-xf-control', formControl.parents('form')).each(function (i) {
+		$(this).recalculate();
+	});
+});
+
+
 })(jQuery);
