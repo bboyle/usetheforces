c3ad0e7db82efd77fa38ec35883a90fa6f093106
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-03-31 12:13:05 +0000

    support for detecting required, blank, valid and invalid controls (console debugging on)

--- jquery.forces.forms.js
+++ jquery.forces.forms.js
@@ -1,83 +1,128 @@
-/** 
- * jquery.forces.forms.js 
- * @see http://usetheforces.googlecode.com/ 
- * @license GNU GENERAL PUBLIC LICENSE Version 3 <http://www.gnu.org/licenses/gpl.html> 
- * @requires jQuery 
+/**
+ * jquery.forces.forms.js
+ * @see http://usetheforces.googlecode.com/
+ * @license GNU GENERAL PUBLIC LICENSE Version 3 <http://www.gnu.org/licenses/gpl.html>
+ * @requires jQuery
  */
 
 (function($){
 
-// selectors 
-$.extend($.expr[':'], { 
-	'-xf-control': function(e) { 
-		return $(e).is('.xf-input,.xf-select1,.xf-group'); 
-	}, 
-	'-xf-group': function(e) { 
-		return $(e).hasClass('xf-group'); 
-	}, 
-	'-xf-hint': function(e) { 
-		return $(e).hasClass('xf-hint'); 
-	}, 
+// selectors
+$.extend($.expr[':'], {
+	// (use)theforces
+	'-tf-blank': function(e) {
+		return $.trim($(e).xfValue()).length == 0;
+	},
+	// xforms
+	'-xf-control': function(e) {
+		return $(e).is('.xf-input,.xf-select1,.xf-group');
+	},
+	'-xf-group': function(e) {
+		return $(e).hasClass('xf-group');
+	},
+	'-xf-hint': function(e) {
+		return $(e).hasClass('xf-hint');
+	},
 	'-xf-input': function(e) { 
 		return $(e).hasClass('xf-input'); 
-	}, 
+	},
+	'-xf-invalid' : function(e) {
+		return $(e).is(':not(:-xf-valid)');
+	},
 	'-xf-label': function(e) { 
 		return $(e).hasClass('xf-label'); 
 	}, 
-	'-xf-select1': function(e) { 
-		return $(e).hasClass('xf-select1'); 
-	}, 
+	'-xf-relevant': function(e) {
+		return $(e).relevant();
+	},
+	'-xf-required': function(e) {
+		return $(e).find('.required').size() > 0;
+	},
+	'-xf-select1': function(e) {
+		return $(e).hasClass('xf-select1');
+	},
+	'-xf-valid': function(e) {
+		return $(e).validate();
+	}
+	
 });
 
 
-$.fn.extend({ 
-	// get form control 
-	formControl: function() { 
-		return this.is(':-xf-control') ? this : this.parents(':-xf-control').eq(0); 
+$.fn.extend({
+	// get form control
+	formControl: function() {
+		return this.is(':-xf-control') ? this : this.parents(':-xf-control').eq(0);
 	},
 
-	// recalculate 
-	recalculate: function() { 
-		this.relevant(); 
+	// get/set relevance
+	relevant: function(expression) {
+		var formControl = this.formControl();
+		if (expression) {
+			formControl.data('relevant', expression);
+		} else {
+			expression = formControl.data('relevant', expression) || true;
+		}
+		var relevant = true;
+		if (typeof(expression) == "function") {
+			relevant = expression(this);
+		} else if (expression) {
+			relevant = expression;
+		}
+		if (relevant != true) {
+			// TODO +filter from jquery object
+			formControl.hide();
+			return false;
+		}
+		formControl.slideDown();
+		return true;
 	},
-
-	// get/set relevance 
-	relevant: function(expression) { 
-		var formControl = this.formControl(); 
-		if (expression) { 
-			formControl.data('relevant', expression); 
-		} else { 
-			expression = formControl.data('relevant', expression) || true; 
+	
+	// is control valid
+	validate: function() {
+		if (this.is(':-tf-blank')) {
+			return !this.is(':-xf-required');
+		// } else { TODO constraint validation			
 		}
-		
-		var relevant = true; 
-		if (typeof(expression) == "function") { 
-			relevant = expression(this); 
-		} else if (expression) { 
-			relevant = expression; 
+		return true;
+	},
+	
+	// get value
+	xfValue: function() {
+		if (this.find('input:text').size()) {
+			return this.find('input:text').val();
 		}
-		
-		if (relevant != true) { 
-			// TODO +filter from jquery object 
-			formControl.hide(); 
-			return false; 
-		}
-		
-		formControl.slideDown(); 
-		return true; 
-	} 
+		// TODO support textarea, password, radio/checkboxes and select
+		return null;
+	}
 });
 
 
-// form control changes 
-$('form.forces').change(function(eventObject) { 
-var formControl = $(eventObject.target).formControl();
-	// recalculate all controls 
-	// TODO rather than use "each", make functions work with jQuery object 
-	$(':-xf-control', formControl.parents('form')).each(function() { 
-		$(this).recalculate(); 
-	}); 
-});
+// form events
+$('form.forces')
+	// form control changed
+	.change(function(eventObject) {
+		var formControl = $(eventObject.target).formControl();
+		// evaluate relevance of all controls
+		$(':-xf-control:-xf-relevant', formControl.parents('form'));
+	})
+	// form was submitted
+	.submit(function(eventObject) {
+		console.log('submit');
+
+		var form = $(eventObject.target);
+
+		console.log($(':-xf-control', form).size(), "fields");
+		console.log($(':-xf-control:-xf-relevant', form).size(), "relevant fields");
+		console.log($(':-xf-control:-xf-required', form).size(), "required fields");
+		console.log($(':-xf-control:-tf-blank', form).size(), "blank fields");
+		console.log($(':-xf-control:-xf-valid', form).size(), "valid fields");
+		console.log($(':-xf-control:-xf-relevant:-xf-invalid', form).size(), "invalid (relevant) fields");
+		
+		// highlight (relevant) invalid controls
+		$(':-xf-control:-xf-relevant:-xf-invalid', form).css('background', 'red');
+
+		return false;
+	});
 
 
 })(jQuery);
