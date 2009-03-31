8c67fcbbeecd336c749cec1c7097d77ecfeccad2
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-03-31 10:59:53 +0000

    support for -xf-select1, -xf-group

--- trunk/jquery.forces.forms.js
+++ trunk/jquery.forces.forms.js
@@ -1,76 +1,82 @@
-// jquery.forces.forms.js
-// usetheforces.googlecode.com
+/** 
+ * jquery.forces.forms.js 
+ * @see http://usetheforces.googlecode.com/ 
+ * @license GNU GENERAL PUBLIC LICENSE Version 3 <http://www.gnu.org/licenses/gpl.html> 
+ * @requires jQuery 
+ */
 
 (function($){
 
-// selectors
-$.extend($.expr[':'], {
-	'-xf-control': function(e) {
-		return $(e).is('.xf-input');
-	},
-	'-xf-hint': function(e) {
-		return $(e).hasClass('xf-hint');
-	},
-	'-xf-input': function(e) {
-		return $(e).hasClass('xf-input');
-	},
-	'-xf-label': function(e) {
-		return $(e).hasClass('xf-label');
-	},
+// selectors 
+$.extend($.expr[':'], { 
+	'-xf-control': function(e) { 
+		return $(e).is('.xf-input,.xf-select1,.xf-group'); 
+	}, 
+	'-xf-group': function(e) { 
+		return $(e).hasClass('xf-group'); 
+	}, 
+	'-xf-hint': function(e) { 
+		return $(e).hasClass('xf-hint'); 
+	}, 
+	'-xf-input': function(e) { 
+		return $(e).hasClass('xf-input'); 
+	}, 
+	'-xf-label': function(e) { 
+		return $(e).hasClass('xf-label'); 
+	}, 
+	'-xf-select1': function(e) { 
+		return $(e).hasClass('xf-seelct1'); 
+	}, 
 });
 
 
-$.fn.extend({
-	// get form control
-	formControl: function() {
-		return this.is(':-xf-control') ? this : this.parents(':-xf-control').eq(0);
+$.fn.extend({ 
+	// get form control 
+	formControl: function() { 
+		return this.is(':-xf-control') ? this : this.parents(':-xf-control').eq(0); 
 	},
 
-	// recalculate
-	recalculate: function() {
-		if (this.relevant()) this.show();
-		else this.hide();
+	// recalculate 
+	recalculate: function() { 
+		this.relevant(); 
 	},
 
-	// get/set relevance
-	relevant: function(expression) {
-		var formControl = this.formControl();
-		if (expression) {
-			formControl.data('relevant', expression);
-		} else {
-			expression = formControl.data('relevant', expression);
-		}
-
-		var relevant = true;
-		if (typeof(expression) == "function") {
-			relevant = expression(this);
-		} else if (expression) {
-			relevant = expression;
+	// get/set relevance 
+	relevant: function(expression) { 
+		var formControl = this.formControl(); 
+		if (expression) { 
+			formControl.data('relevant', expression); 
+		} else { 
+			expression = formControl.data('relevant', expression) || true; 
 		}
 		
-		if (relevant != true) {
-			// TODO +filter from jquery object
-			formControl.hide()
-			return false;
+		var relevant = true; 
+		if (typeof(expression) == "function") { 
+			relevant = expression(this); 
+		} else if (expression) { 
+			relevant = expression; 
 		}
 		
-		// default
-		formControl.show();
-		return true;
-	}
+		if (relevant != true) { 
+			// TODO +filter from jquery object 
+			formControl.hide(); 
+			return false; 
+		}
+		
+		formControl.slideDown(); 
+		return true; 
+	} 
 });
 
 
-// form control changes
-$('form.forces').change(function(eventObject) {
-
-	var formControl = $(eventObject.target).formControl();
-
-	// recalculate all controls
-	// TODO rather than use "each", make functions work with jQuery object
-	$(':-xf-control', formControl.parents('form')).each(function (i) {
-		$(this).recalculate();
-	});
+// form control changes 
+$('form.forces').change(function(eventObject) { 
+var formControl = $(eventObject.target).formControl();
+	// recalculate all controls 
+	// TODO rather than use "each", make functions work with jQuery object 
+	$(':-xf-control', formControl.parents('form')).each(function() { 
+		$(this).recalculate(); 
+	}); 
 });
 
 
