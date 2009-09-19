05008eb13864265a92b960fd60987618d924fd6b
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-09-19 00:16:02 +0000

    basic UI tests, identify "control elements"

--- jquery.forces.forms.ui.js
+++ jquery.forces.forms.ui.js
@@ -8,15 +8,35 @@
 ;if(typeof(jQuery)!="undefined") {
 
 (function($){
-
+	// jquery.forces
 	var $F = $.forces = $.forces || {};
 
+
+// selectors
+$.extend($.expr[':'], {
+	'-xf-control': function(e) {
+		return $(e).is('.xf-input,.xf-select');
+	}
+});
+
+
+// find "control" element
+$.fn.forces_control = function() {
+	return this.map(function() {
+		var e = $(this);
+		return (e.is(':-xf-control') ? e : e.parents(':-xf-control')).get(0);
+	});
+};
+
+
+/*
 $(document).bind($.forces.EVENT_REQUIRED, function(evt) {
 	$(evt.target).before('<abbr class="required" title="required">*</abbr>');
 });
 $(document).bind($.forces.EVENT_OPTIONAL, function(evt) {
 	$(evt.target).prev('abbr.required').remove();
 });
+*/
 
 
 })(jQuery);
