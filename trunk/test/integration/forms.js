40f907fe8bbf3b430dd067b6beac27542e56b62e
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-10-13 12:23:38 +0000

    update to YUI3 for testing; added submit suppression (10 second tolerance)

--- trunk/test/integration/forms.js
+++ trunk/test/integration/forms.js
@@ -1,77 +1,93 @@
-//create the test suite
-var TS_formsIntegration = new YAHOO.tool.TestSuite("forces.forms integration test suite"); 
+Tester.use('console', 'test', function(Y){
+	var Assert = Y.Assert;
+
+	Y.namespace('forces.test');
+
+	//create the test suite
+	Y.forces.test.FormIntegrationSuite = new Y.Test.Suite("forces.forms integration test suite"); 
+
+	// form events integration tests
+	Y.forces.test.FormIntegrationSuite.add(new Y.Test.Case({
+		name: "Form events integration tests",
+
+		//---------------------------------------------
+		// Setup and tear down
+		//---------------------------------------------
+
+		setUp: function () {
+			$(
+				'<form id="form" action="#form"><ol>' +
+					'<li><input type="text" name="input01" id="input01" /></li>' +
+					'<li><input type="text" name="input02" id="input02" /></li>' +
+				'</ol></form>'
+			).appendTo('body');
+		},
+
+		tearDown: function () {
+			$('#form').remove();
+		},
+
+		//---------------------------------------------
+		// Tests
+		//---------------------------------------------
+
+		test_relevantEventsFireCorrectly: function () {
+			$(document).bind($.forces.EVENT_RELEVANT, function(evt, target) {
+				target.before('<span class="relevant">RELEVANT</span>');
+			});
+			$(document).bind($.forces.EVENT_IRRELEVANT, function(evt, target) {
+				target.before('<span class="relevant">IRRELEVANT</span>');
+			});
+
+			Assert.areSame(0, $('#input01').prev('.relevant').length);
+			Assert.areSame(0, $('#input01').forces_attr('relevant', true).prev('.relevant').length, 'RELEVANT event fired when relevance did not change');
+			Assert.areSame(1, $('#input01').forces_attr('relevant', false).forces_attr('relevant', true).prev('.relevant').length, 'RELEVANT event not detected');
+			Assert.areSame('RELEVANT', $('#input01').prev('.relevant').text());
+
+			Assert.areSame(0, $('#input02').prev('.relevant').length);
+			Assert.areSame(1, $('#input02').forces_attr('relevant', false).prev('.relevant').length);
+			Assert.areSame('IRRELEVANT', $('#input02').prev('.relevant').text());
+		},
 
 
-var Assert = YAHOO.util.Assert;
+		test_requiredEventsFireCorrectly: function () {
+			$(document).bind($.forces.EVENT_REQUIRED, function(evt, target) {
+				target.before('<span class="required">REQUIRED</span>');
+			});
+			$(document).bind($.forces.EVENT_OPTIONAL, function(evt, target) {
+				target.before('<span class="required">OPTIONAL</span>');
+			});
+
+			Assert.areSame(0, $('#input01').prev('.required').length);
+			Assert.areSame(1, $('#input01').forces_attr('required', true).prev('.required').length);
+			Assert.areSame('REQUIRED', $('#input01').prev('.required').text());
+
+			Assert.areSame(0, $('#input02').prev('.required').length);
+			Assert.areSame(0, $('#input02').forces_attr('required', false).prev('.required').length, 'OPTIONAL event fired when required state did not change');
+			Assert.areSame(1, $('#input02').forces_attr('required', true).forces_attr('required', false).prev('.required').length);
+			Assert.areSame('OPTIONAL', $('#input02').prev('.required').text());
+		},
 
 
-// required fields unit tests
-var TC_formEventIntegration = new YAHOO.tool.TestCase({
+		test_submitEventSuppression: function () {
+			var submitted = 0;
+			$(document).submit(function() { ++submitted; });
+			
+			function wasSubmitted(wasSubmitted) {
+				$('#form').submit();
+				return submitted > wasSubmitted;
+			}
+			
+			Assert.areSame(true, wasSubmitted(submitted));
+			Assert.areSame(false, wasSubmitted(submitted), 'repeated submit not suppressed'); 
+			this.wait(function(){ 
+				Assert.areSame(true, wasSubmitted(submitted), 'submit after tolerance should proceed'); 
+			}, $.forces.SUBMIT_TOLERANCE + 100); 
+		}
+	}));
 
-	name: "Form events integration tests",
 
-	//---------------------------------------------
-	// Setup and tear down
-	//---------------------------------------------
+	//add the test suite
+	Y.Test.Runner.add(Y.forces.test.FormIntegrationSuite);
 
-	setUp: function () {
-		$(
-			'<form id="form" action="#form"><ol>' +
-				'<li><input type="text" name="input01" id="input01" /></li>' +
-				'<li><input type="text" name="input02" id="input02" /></li>' +
-			'</ol></form>'
-		).appendTo('body');
-	},
-
-	tearDown: function () {
-		$('#form').remove();
-	},
-
-	//---------------------------------------------
-	// Tests
-	//---------------------------------------------
-
-	test_relevantEventsFireCorrectly: function () {
-		$(document).bind($.forces.EVENT_RELEVANT, function(evt, target) {
-			target.before('<span class="relevant">RELEVANT</span>');
-		});
-		$(document).bind($.forces.EVENT_IRRELEVANT, function(evt, target) {
-			target.before('<span class="relevant">IRRELEVANT</span>');
-		});
-
-		Assert.areSame(0, $('#input01').prev('.relevant').length);
-		Assert.areSame(0, $('#input01').forces_attr('relevant', true).prev('.relevant').length, 'RELEVANT event fired when relevance did not change');
-		Assert.areSame(1, $('#input01').forces_attr('relevant', false).forces_attr('relevant', true).prev('.relevant').length, 'RELEVANT event not detected');
-		Assert.areSame('RELEVANT', $('#input01').prev('.relevant').text());
-
-		Assert.areSame(0, $('#input02').prev('.relevant').length);
-		Assert.areSame(1, $('#input02').forces_attr('relevant', false).prev('.relevant').length);
-		Assert.areSame('IRRELEVANT', $('#input02').prev('.relevant').text());
-	},
-	
-
-	test_requiredEventsFireCorrectly: function () {
-		$(document).bind($.forces.EVENT_REQUIRED, function(evt, target) {
-			target.before('<span class="required">REQUIRED</span>');
-		});
-		$(document).bind($.forces.EVENT_OPTIONAL, function(evt, target) {
-			target.before('<span class="required">OPTIONAL</span>');
-		});
-
-		Assert.areSame(0, $('#input01').prev('.required').length);
-		Assert.areSame(1, $('#input01').forces_attr('required', true).prev('.required').length);
-		Assert.areSame('REQUIRED', $('#input01').prev('.required').text());
-
-		Assert.areSame(0, $('#input02').prev('.required').length);
-		Assert.areSame(0, $('#input02').forces_attr('required', false).prev('.required').length, 'OPTIONAL event fired when required state did not change');
-		Assert.areSame(1, $('#input02').forces_attr('required', true).forces_attr('required', false).prev('.required').length);
-		Assert.areSame('OPTIONAL', $('#input02').prev('.required').text());
-	}
 });
-
-
-//add test cases
-TS_formsIntegration.add(TC_formEventIntegration);
-
-//add the test suite
-YAHOO.tool.TestRunner.add(TS_formsIntegration);
