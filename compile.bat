5f659321d87ecfefb876b49d7ed5dbf63399f2ba
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2009-11-17 04:08:13 +0000

    space out alert messages

--- compile.bat
+++ compile.bat
@@ -1,2 +1,3 @@
 copy /b license.js + jquery.forces.date.js + jquery.forces.xpath.js + jquery.forces.dom.js + jquery.forces.fx.js + jquery.forces.forms.core.js + jquery.forces.forms.ui.js jquery.forces.forms.combined.js
 java -jar ..\yuicompressor-2.4.2.jar jquery.forces.forms.combined.js -o jquery.forces.forms.min.js
+java -jar ..\yuicompressor-2.4.2.jar forces.forms.css -o forces.forms.min.css
