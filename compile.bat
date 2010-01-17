199002725cd72d9c08c5ab4d3cad6244d7adf16d
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2010-01-17 06:10:59 +0000

    introducing google compiler minification (for comparison with YUI compressor)

--- compile.bat
+++ compile.bat
@@ -1,4 +1,8 @@
 copy /b license.js + jquery.forces.date.js + jquery.forces.xpath.js + jquery.forces.dom.js + jquery.forces.fx.js + jquery.forces.forms.core.js + jquery.forces.forms.ui.js jquery.forces.forms.combined.js
-java -jar ..\yuicompressor-2.4.2.jar jquery.forces.forms.combined.js -o jquery.forces.forms.min.js
+
+java -jar ..\compiler.jar -js jquery.forces.forms.combined.js -js_output_file jquery.forces.forms.min-google.js
+java -jar ..\yuicompressor-2.4.2.jar jquery.forces.forms.combined.js -o jquery.forces.forms.min-yui.js
+copy jquery.forces.forms.min-yui.js jquery.forces.forms.min.js
+
 java -jar ..\yuicompressor-2.4.2.jar jquery.forces.lib.validate.js -o jquery.forces.lib.validate.min.js
 java -jar ..\yuicompressor-2.4.2.jar forces.forms.css -o forces.forms.min.css
