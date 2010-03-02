55884ae545ed70b464d649b25a23e36c0f336446
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2010-03-02 11:06:53 +0000

    simplify test pages, tests-setup.js now writes all script tags for unit/integration test files

--- trunk/test/tests-setup.js
+++ trunk/test/tests-setup.js
@@ -1,3 +1,11 @@
+document.write('<script type="text/javascript" src="unit/date.js"></script>');
+document.write('<script type="text/javascript" src="unit/xpath.js"></script>');
+document.write('<script type="text/javascript" src="unit/dom.js"></script>');
+document.write('<script type="text/javascript" src="unit/forms.js"></script>');
+document.write('<script type="text/javascript" src="unit/forms.validate.js"></script>');
+document.write('<script type="text/javascript" src="unit/forms.ui.js"></script>');
+document.write('<script type="text/javascript" src="integration/forms.js"></script>');
+
 Tester = YUI({ logInclude: { TestRunner: true } }).use('console-filters', 'test', function(Y){
 	//initialize the console
 	new Y.Console({
