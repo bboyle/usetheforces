eef7d163bb7ec591ea2fa98e8be51f68d4a666f0
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2010-03-12 10:38:28 +0000

    RANGE-001 preserve attributes: id, class, name, title

--- test/unit/forms.range.js
+++ test/unit/forms.range.js
@@ -19,17 +19,17 @@ Tester.use('console', 'test', function(Y){
 				'<div id="form-container" class="tf-form">'+
 				'<form id="form" action="#form" onsubmit="return false">' +
 				'<div>' +
-				'<select id="select1">' +
+				'<select id="select1" name="form.selectOne" class="red blue">' +
 					'<option value="1">one</option>' +
 					'<option value="2" selected="selected">two</option>' +
 					'<option value="3">three</option>' +
 				'</select>' +
-				'<select id="select2">' +
+				'<select id="select2" name="form.selectTwo" title="test select list">' +
 					'<option>0.2</option>' +
 					'<option>0.5</option>' +
 					'<option>0.1</option>' +
 				'</select>' +
-				'<select id="select3">' +
+				'<select id="select3" name="form.selectThree">' +
 					'<option value="1">one</option>' +
 					'<option value="abc">abc</option>' +
 					'<option value="3">three</option>' +
@@ -115,6 +115,55 @@ Tester.use('console', 'test', function(Y){
 				var select = $('#select1,#select2').forces_toRange();
 				Assert.areSame('2', select.eq(0).val());
 				Assert.areSame('0.2', select.eq(1).val());
+				
+			} else {
+				Y.log('Input type range IS NOT supported in this browser', 'info', 'TestRunner');
+			}
+		},
+		
+		test_idAttributeCopiedFromSelect: function() {
+			if (isRangeSupported) {
+				
+				var select = $('#select1,#select2').forces_toRange();
+				Assert.areSame('select1', select.eq(0).attr('id'));
+				Assert.areSame('select2', select.eq(1).attr('id'));
+				
+			} else {
+				Y.log('Input type range IS NOT supported in this browser', 'info', 'TestRunner');
+			}
+		},
+		
+		test_nameAttributeCopiedFromSelect: function() {
+			if (isRangeSupported) {
+				
+				var select = $('#select1,#select2').forces_toRange();
+				Assert.areSame('form.selectOne', select.eq(0).attr('name'));
+				Assert.areSame('form.selectTwo', select.eq(1).attr('name'));
+				
+			} else {
+				Y.log('Input type range IS NOT supported in this browser', 'info', 'TestRunner');
+			}
+		},
+		
+		test_classAttributeCopiedFromSelect: function() {
+			if (isRangeSupported) {
+				
+				var select = $('#select1,#select2').forces_toRange();
+				Assert.areSame('red blue', select.eq(0).attr('class'));
+				Assert.areSame(true, select.eq(0).hasClass('red'));
+				Assert.areSame('', select.eq(1).attr('class'));
+				
+			} else {
+				Y.log('Input type range IS NOT supported in this browser', 'info', 'TestRunner');
+			}
+		},
+		
+		test_titleAttributeCopiedFromSelect: function() {
+			if (isRangeSupported) {
+				
+				var select = $('#select1,#select2').forces_toRange();
+				Assert.areSame('', select.eq(0).attr('title'));
+				Assert.areSame('test select list', select.eq(1).attr('title'));
 				
 			} else {
 				Y.log('Input type range IS NOT supported in this browser', 'info', 'TestRunner');
