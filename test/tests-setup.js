a325c40a5554dc19b411bb9fc9cecf817674bcae
benjamins.boyle <benjamins.boyle@19932d04-1a05-11de-bcda-2bb9902f8029>
2010-04-23 13:34:48 +0000

    updated to YUI 3.1.0 library for tests

--- test/tests-setup.js
+++ test/tests-setup.js
@@ -1,3 +1,15 @@
+Tester = YUI({ logInclude: { TestRunner: true } }).use('console-filters', 'test', function(Y) {
+	//initialize the console
+	new Y.Console({
+		logLevel: 'debug',
+		height: '40em',
+		newestOnTop: false,
+		plugins: [ Y.Plugin.ConsoleFilters ]
+	}).render('#log');
+
+	$('body').addClass('yui3-skin-sam');
+});
+
 document.write('<script type="text/javascript" src="unit/date.js"></script>');
 document.write('<script type="text/javascript" src="unit/xpath.js"></script>');
 document.write('<script type="text/javascript" src="unit/dom.js"></script>');
@@ -6,14 +18,3 @@ document.write('<script type="text/javascript" src="unit/forms.validate.js"></sc
 document.write('<script type="text/javascript" src="unit/forms.ui.js"></script>');
 document.write('<script type="text/javascript" src="unit/forms.range.js"></script>');
 document.write('<script type="text/javascript" src="integration/forms.js"></script>');
-
-
-Tester = YUI({ logInclude: { TestRunner: true } }).use('console-filters', 'test', function(Y){
-	//initialize the console
-	new Y.Console({
-		logLevel: 'debug',
-		height: '40em',
-		newestOnTop: false,
-		plugins: [ Y.Plugin.ConsoleFilters ]
-	}).render('#log');
-});
